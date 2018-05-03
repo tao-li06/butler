69e8dfd4b59041c16706ea4f63f590398c7d57f4
tao-li06 <litao006@gmail.com>
2018-05-02 22:33:44 -0700

    Remove docker container if it exists

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker container rm butler && docker run  -d -it -p 3000:3000 --name=butler butler
+name='butler'
+
+[[ $(docker ps -f "name=$name" --format '{{.Names}}') == $name ]] || docker container rm butler
+
+docker run  -d -it -p 3000:3000 --name=butler butler
